8b70b4d6cb7df101f7ecce81a2f9cf8b99eea9ac
Shahzaib <shahzaibux@gmail.com>
2020-05-16 23:38:03 +0500

    Created Continue Statement

---
+++ LearnPHP/ContinueStatement.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        for ($i = 1; $i <= 10 ; $i++) { 
+            if($i == 3)
+            {
+                //echo "No. :" . $i . "<br>";
+                continue; // for skip 3
+            }
+            echo "Number : " . $i . "<br>";
+        }
+
+
+        //Break
+        for ($i = 1; $i <= 10 ; $i++) { 
+            if($i == 3)
+            {
+                echo "No. :" . $i . "<br>";
+            break;
+            }
+            echo "Number : " . $i . "<br>";
+        }
+
+    ?>
+</body>
+</html>
