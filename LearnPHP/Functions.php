535d2bee46256357edaae0c1327d70882cac1652
Shahzaib <shahzaibux@gmail.com>
2020-05-18 02:50:15 +0500

    Created Function Argument By Reference

--- LearnPHP/Functions.php
+++ LearnPHP/Functions.php
@@ -89,6 +89,35 @@
 
         echo $total;
 
+
+
+        //Function Argument By Reference
+        function testing($string){
+            $string .= "and something extra";
+        }
+
+        $str = "This is a string";
+        testing($str);
+        echo $str;
+
+
+        function first($num){
+            $num += 5;
+        }
+
+        function second(&$num){
+            $num += 7;
+        }
+
+        $number = 10;
+        first($number);
+        echo "Orignal Value is $number<br>";
+
+        second($number);
+        echo "Orignal Value is $number<br>";
+
+        
+
     ?>
 </body>
 </html>
