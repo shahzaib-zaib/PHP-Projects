7b65e85b13979ab9d371287e550c99ff19ca7232
Shahzaib <shahzaibux@gmail.com>
2020-05-19 02:01:48 +0500

    Multidimensional Array

--- LearnPHP/Array.php
+++ LearnPHP/Array.php
@@ -109,6 +109,72 @@
         }
         echo "</ul>";
 
+
+
+        //Multidimensional Array
+
+        $emp = [
+            [1,"Shahzaib","Director","300000"],
+            [2,"Salman","Salesman","20000"],
+            [3,"Mohan","Computer Operator","12000"],
+            [4,"Amir","Driver","5000"],
+        ];
+
+        echo $emp[0][0] . " ";
+        echo $emp[0][1] . " ";
+        echo $emp[0][2] . " ";
+        echo $emp[0][3] . " ";
+        echo "<br>";
+        echo $emp[1][0] . " ";
+        echo $emp[1][1] . " ";
+        echo $emp[1][2] . " ";
+        echo $emp[1][3] . " ";
+
+
+        echo "<pre>";
+        print_r($emp);
+        echo "</pre>";
+
+
+        $emp = [
+            [1,"Shahzaib","Director","300000"],
+            [2,"Salman","Salesman","20000"],
+            [3,"Mohan","Computer Operator","12000"],
+            [4,"Amir","Driver","5000"],
+        ];
+
+       for ($row=0; $row < 4; $row++) { 
+           for ($col=0; $col < 4; $col++) { 
+               echo $emp[$row][$col] . " ";
+           }
+           echo "<br>";
+       }
+
+
+       $emp = [
+        [1,"Shahzaib","Director","300000"],
+        [2,"Salman","Salesman","20000"],
+        [3,"Mohan","Computer Operator","12000"],
+        [4,"Amir","Driver","5000"],
+    ];
+
+    echo "<table border= '2px' cellpadding='5px' cellspacing='0'>";
+    echo "<tr>
+            <th>Emp Id</th>
+            <th>Emp Name</th>
+            <th>Emp Designation</th>
+            <th>Emp Salary</th>
+
+        </tr>";
+   foreach ($emp as $v1) {
+       echo "<tr>";
+    foreach ($v1 as $v2) {
+        echo "<td> $v2 </td>";
+    }
+    echo "<tr>";
+   }
+   echo "</table>";
+
     ?>
 </body>
 </html>
