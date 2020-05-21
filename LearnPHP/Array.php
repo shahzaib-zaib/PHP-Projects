e30ec6395df33e3e9140dc27a94fc143ef1c9ede
Shahzaib <shahzaibux@gmail.com>
2020-05-21 23:21:38 +0500

    Created List and Associative Array

--- LearnPHP/Array.php
+++ LearnPHP/Array.php
@@ -178,6 +178,7 @@
 
    //Multidimensional Array With List
 
+   //Index Array
    $emp = [
     [1,"Shahzaib","Director","300000"],
     [2,"Salman","Salesman","20000"],
@@ -185,58 +186,57 @@
     [4,"Amir","Driver","5000"],
     ];
 
-    echo $emp[0][0] . " ";
-    echo $emp[0][1] . " ";
-    echo $emp[0][2] . " ";
-    echo $emp[0][3] . " ";
-    echo "<br>";
-    echo $emp[1][0] . " ";
-    echo $emp[1][1] . " ";
-    echo $emp[1][2] . " ";
-    echo $emp[1][3] . " ";
+    echo "<table border='1px' cellpadding='5px'> cellspacing='0px'
+            <tr>
+                <th>Emp Id</th>
+                <th>Name</th>
+                <th>Designation</th>
+                <th>Salary</th>
+            </tr>
+    ";
+    foreach ($emp as list($id, $name, $desig, $salery)) {
+        echo "<tr><td>$id</td> <td>$name</td> <td>$desig</td> <td>$salery</td></tr>";
+    }
+    echo "</table>";
 
 
-    echo "<pre>";
-    print_r($emp);
-    echo "</pre>";
 
-
+    //Associative Array
     $emp = [
-        [1,"Shahzaib","Director","300000"],
-        [2,"Salman","Salesman","20000"],
-        [3,"Mohan","Computer Operator","12000"],
-        [4,"Amir","Driver","5000"],
-    ];
+        [
+            "id" => 1,
+            "name" => "Kamran",
+            "designation" => "Manager",
+            "salary" => 50000
+        ],
+        [
+            "id" => 2,
+            "name" => "Faizan",
+            "designation" => "Manager",
+            "salary" => 50000
+        ],[
+            "id" => 3,
+            "name" => "Tariq",
+            "designation" => "Manager",
+            "salary" => 50000
+        ],[
+            "id" => 4,
+            "name" => "Azhar",
+            "designation" => "Manager",
+            "salary" => 50000
+        ],
+    ]
 
-    for ($row=0; $row < 4; $row++) { 
-    for ($col=0; $col < 4; $col++) { 
-        echo $emp[$row][$col] . " ";
-    }
-    echo "<br>";
-    }
-
-
-    $emp = [
-    [1,"Shahzaib","Director","300000"],
-    [2,"Salman","Salesman","20000"],
-    [3,"Mohan","Computer Operator","12000"],
-    [4,"Amir","Driver","5000"],
-    ];
-
-    echo "<table border= '2px' cellpadding='5px' cellspacing='0'>";
-    echo "<tr>
-        <th>Emp Id</th>
-        <th>Emp Name</th>
-        <th>Emp Designation</th>
-        <th>Emp Salary</th>
-
-    </tr>";
-    foreach ($emp as $v1) {
-    echo "<tr>";
-    foreach ($v1 as $v2) {
-    echo "<td> $v2 </td>";
-    }
-    echo "<tr>";
+    echo "<table border='1px' cellpadding='5px'> cellspacing='0px'
+            <tr>
+                <th>Emp Id</th>
+                <th>Name</th>
+                <th>Designation</th>
+                <th>Salary</th>
+            </tr>
+    ";
+    foreach ($emp as list("id" => $id, "name" => $name, "designation" =>  $desig, "salary" => $salery)) {
+        echo "<tr><td>$id</td> <td>$name</td> <td>$desig</td> <td>$salery</td></tr>";
     }
     echo "</table>";
 
