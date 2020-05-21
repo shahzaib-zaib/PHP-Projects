ecd1450caa42d89710c831a648f0202714cdb7b7
Shahzaib <shahzaibux@gmail.com>
2020-05-22 01:21:11 +0500

    Created Replace Function

--- LearnPHP/Array.php
+++ LearnPHP/Array.php
@@ -301,7 +301,39 @@
 
     echo array_search('apple', $food);
 
+
+
+    //=================== Array_replace & Array_replace_recursive Function ===================
+
+    $fruit = ['orange', 'banana', 'apple', 'grapes'];
+    $veggie = ['carrot', 'pea'];
+    $color = ['red', 'green', 'blue'];
+
+    $newArray = array_replace($fruit, $veggie, $color);
+
+    echo "<pre>";
+    print_r($newArray);
+    echo "</pre>";
+
+
+    $fruit = ['orange', 'banana', 'apple', 'grapes'];
+    $veggie = ['a' => 'carrot', 'b'=> 'pea'];
+
+    $newArray = array_replace($fruit, $veggie);
+
+    echo "<pre>";
+    print_r($newArray);
+    echo "</pre>";
+
+
+    $array1 = array("a" => array("red"), "b" => array("green", "pink" ));
+    $array2 = array("a" => array("yellow"), "b" => array("black"));
     
+    $newArray = array_replace_recursive($array1, $array2);
+
+    echo "<pre>";
+    print_r($newArray);
+    echo "</pre>";
 
     ?>
 </body>
