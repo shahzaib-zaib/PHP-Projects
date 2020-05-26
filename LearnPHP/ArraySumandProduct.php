faed7c337cbc047b460b5cfd54accf1fb6862955
Shahzaib <shahzaibux@gmail.com>
2020-05-26 06:49:40 +0500

    Created Array Sum and Product Function

---
+++ LearnPHP/ArraySumandProduct.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        //$a = array(2, 4, 6, 8);
+        $a = array("a" => 1.2, "b" => 2.3, "c" => 3.4);
+
+        echo "Sum = " . array_sum($a);
+
+
+        //$a = array(2, 4, 6, 8);
+        $a = array("a" => 1.2, "b" => 2.3, "c" => 3.4);
+
+        echo "Product = " . array_product($a);
+
+
+    ?>
+</body>
+</html>
