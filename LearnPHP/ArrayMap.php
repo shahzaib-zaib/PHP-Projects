1b3a4aee64fb30c3937d37c73bc6f090b978edb2
Shahzaib <shahzaibux@gmail.com>
2020-05-28 22:29:39 +0500

    Created Array Map Function

--- LearnPHP/ArrayMap.php
+++ LearnPHP/ArrayMap.php
@@ -6,6 +6,38 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+
+        function square($n, $m){
+            //return "$n for $m";
+            return [$n => $m];
+        }
+
+        $a = [1,2,3,4,5];
+        $b = ['lemon', 'orange', 'banana', 'apple', 'guava'];
+
+        $newArray = array_map('square', $a, $b);
+
+        echo "<pre>";
+        print_r($newArray);
+        echo "</pre>";
+
+
+
+
+
+
+        function square($n){
+            return strtoupper($n);
+        }
+
+        $a = array("one" => "Apple", "two" => "Banana", "three" => "Orange");
+
+        $newArray = array_map('square', $a);
+
+        echo "<pre>";
+        print_r($newArray);
+        echo "</pre>";
+    ?>
 </body>
 </html>
