521a27b89ae25d60f3e30b7875ecf50d6eb28771
Shahzaib <shahzaibux@gmail.com>
2020-05-30 14:22:45 +0500

    Created Login Page and Style sheet

---
+++ Facebook/Loginpage.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="_li">
+        <div class="_3_s0 _1toe _3_s1 _3_s1 uiBoxGray noborder">
+            <div class="_608m">
+                <div class="_5aj7 _tb6">
+                    <div class="_53jh">
+                        <div class="loggedout_menubar_container">
+                            <div class="clearfix loggedout_menubar">
+                                <div class="lfloat _ohe">
+                                    <h1>
+                                        <a href="#" title="Go to Facebook home">
+                                            <i class="fb_logo img sp_DVvTt_Dqodp sx_b906b8">
+                                                <u>Facebook</u>
+                                            </i>
+                                        </a>
+                                    </h1>
+                                </div>
+                                <div class="menu_login_container rfloat _ohf">
+                                    <form id="login_form" action="#" method="post">
+                                        <table role="presentation" cellspacing="0">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="html7magic">
+                                                        <label for="email">Email or phone</label>
+                                                    </td>
+                                                    <td class="html7magic">
+                                                        <label for="pass">Password</label>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td>
+                                                        <input id="email" name="email" type="email" class="inputtext login_form_input_box" data-testid="royal_email">
+                                                    </td>
+                                                    <td>
+                                                        <input id="pass" type="password" class="inputtext login_form_input_box" name="pass" data-testid="royal_pass">
+                                                    </td>
+                                                    <td>
+                                                        <label id="loginbutton" for="u_0_4" class="login_form_login_button uiButton uiButtonConfirm">
+                                                            <input id="u_0_4" value="Log In" aria-label="Log In" data-testid="royal_login_button" type="submit">
+                                                        </label>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
