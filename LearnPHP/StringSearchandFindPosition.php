3c00a9bffae19a340e5468b1b5242faada364387
Shahzaib <shahzaibux@gmail.com>
2020-06-02 01:37:36 +0500

    Updated

--- LearnPHP/StringSearchandFindPosition.php
+++ LearnPHP/StringSearchandFindPosition.php
@@ -6,6 +6,19 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+
+        $str = "Hello world. The world is nice";
+
+        echo "Strpos : " . strpos($str, "world") . "<br>"; // 6
+        echo "Strpos : " . strpos($str, "world", 10) . "<br>"; // 17
+        echo "Strrpos : " . strrpos($str, "world") . "<br>"; // 17
+        echo "Stripos : " . stripos($str, "world", 10) . "<br>"; // 6
+        echo "Strripos : " . strripos($str, "world") . "<br>"; // 17
+
+
+
+
+    ?>
 </body>
 </html>
