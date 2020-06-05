1aca09091cdeaee93d2e3a73aa32ef56191b2abc
Shahzaib <shahzaibux@gmail.com>
2020-06-05 18:41:11 +0500

    Crud Opretions

--- Company/Read.php
+++ Company/Read.php
@@ -1,3 +1,11 @@
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<!-- Google Fonts -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+<!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.0/css/mdb.min.css" rel="stylesheet">
 <?php
 
     $con = mysqli_connect('localhost:3306', 'root', '', 'companydb');
@@ -7,7 +15,41 @@
 
     echo "<h2>Total Employees in Company : $totalrows</h2>"
 
-    $records = mysqli_fetch_assoc($execute);
 
+    if ($totalrows != 0) {
+    ?>
+        <table class="table table-border table-hover">
+            <tr>
+                <th>Employee Id</th>
+                <th>Employee Name</th>
+                <th>Employee Email</th>
+                <th>Employee Gender</th>
+                <th>Employee Phone Number</th>
+                <th>Employee Qualification</th>
+                <th>Employee Salary</th>
+            </tr>
+            
+    <?php
+            while ($records = mysqli_fetch_assoc($execute)) {
+                echo "<tr>
+                <td>". $records['EmpId'] ."</td>
+                <td>". $records['EmpName'] ."</td>
+                <td>". $records['EmpEmail'] ."</td>
+                <td>". $records['EmpGender'] ."</td>
+                <td>". $records['EmpPhoneNumber'] ."</td>
+                <td>". $records['EmpQualitfication'] ."</td>
+                <td>". $records['EmpSalary'] ."</td>
+            </tr>";
 
-?>
+            }
+            }
+            else 
+            {
+                echo "<h4>Table have no Record</h4>"
+            }
+            
+    ?>
+        </table>
+        <button>
+            <a href="EmployeeForm.html">Register Employee</a>
+        </button>
