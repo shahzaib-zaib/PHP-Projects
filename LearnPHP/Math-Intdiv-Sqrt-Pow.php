e3a2a147cc42f1a06a4f72953529cf89e06170d8
Shahzaib <shahzaibux@gmail.com>
2020-06-09 01:46:25 +0500

    Created Math Intdiv Sqrt and Pow

---
+++ LearnPHP/Math-Intdiv-Sqrt-Pow.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        echo intdiv(8, 4) . "<br>"; // 2
+        echo intdiv(5, 2) . "<br>"; // 2
+        echo intdiv(-5, -2) . "<br>"; // 2
+
+
+
+
+        echo sqrt(9) . "<br>"; // 3
+        echo sqrt(4) . "<br>"; // 2
+        echo sqrt(5) . "<br>"; // 2.23
+        echo sqrt(0) . "<br>"; // 0
+
+
+
+
+        echo pow(2, 2) . "<br>"; // 4
+        echo pow(4, 4) . "<br>"; // 16
+        echo pow(3, 3) . "<br>"; // 9
+        echo pow(-2, 3) . "<br>"; // -8
+        echo pow(-2, 4) . "<br>"; // 16
+        echo pow(-2, -4) . "<br>"; // 0.0625
+
+
+
+
+
+
+    ?>
+</body>
+</html>
