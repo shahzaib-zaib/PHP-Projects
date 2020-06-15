a010f1300fd27d4fbb5ea50825f0a9e7694a3383
Shahzaib <shahzaibux@gmail.com>
2020-06-16 02:04:01 +0500

    Project Initiaied

---
+++ MYSQL/IN-Operator.php
@@ -0,0 +1,8 @@
+select * from table_name where column_name IN ("condition1", "condition2", condition3.....);
+
+select id, name, phone, from table_name where IN ("condition1", "condition2", condition3.....);
+
+
+select * from table_name where column_name NOT IN ("condition1", "condition2", condition3.....);
+
+select id, name, phone, from table_name where NOT IN ("condition1", "condition2", condition3.....);
