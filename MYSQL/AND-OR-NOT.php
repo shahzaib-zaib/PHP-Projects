4fb5d384357e9f9a2a574504f74ad4f66b9f7149
Shahzaib <shahzaibux@gmail.com>
2020-06-16 01:50:21 +0500

    Project Initiaied

---
+++ MYSQL/AND-OR-NOT.php
@@ -0,0 +1,19 @@
+select * from table_name where condition1 AND condition2 AND condition3.....;
+
+select id, name, phone, from table_name where condition1 AND condition2 AND condition3.....;
+
+
+
+
+select * from table_name where condition1 OR condition2 OR condition3.....;
+
+select id, name, phone, from table_name where condition1 OR condition2 OR condition3.....;
+
+
+
+select * from table_name where (city = "Karachi" OR city = "Islamabad") AND gender = "Male";
+
+
+
+select * from table_name where NOT (city = "Karachi" OR city = "Islamabad");
+
