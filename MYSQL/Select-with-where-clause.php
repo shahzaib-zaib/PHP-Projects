c2959fb92a53d3d09b81dae843c969b2c8d57b9d
Shahzaib <shahzaibux@gmail.com>
2020-06-16 01:07:16 +0500

    Project Initiaied

---
+++ MYSQL/Select-with-where-clause.php
@@ -0,0 +1,20 @@
+select * from table_name;
+
+select id, name, phone, from table_name;
+
+select id as Id, name as "Student Name", phone as phone, from table_name;
+
+
+
+
+WHERE Comparison Operators
+
+=           Equal
+>           Greator than
+<           Less than
+>=          Greator than or Equal
+<=          Less than or equal
+<> or !=    Not equal.
+BETWEEN     Between a certain range
+LIKE        Search from a pattern
+IN          To specify multiple possible values from a column
