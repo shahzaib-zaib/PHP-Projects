e148165ca1fb79b499da1b583c55c52ff5829cfa
Shahzaib <shahzaibux@gmail.com>
2020-06-20 19:45:32 +0500

    Project Initiaied

---
+++ RoleBasedAuthentication/Login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <h4>Enter Your Username</h4>
+        <input type="text" name="name">
+        <h4>Enter Your Password</h4>
+        <input type="password" name="pswd">
+
+        
+    </form>
+</body>
+</html>
