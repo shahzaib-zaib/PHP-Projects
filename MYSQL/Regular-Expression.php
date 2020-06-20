708719d8b8eefab4ec198144fd26030612947ae6
Shahzaib <shahzaibux@gmail.com>
2020-06-21 03:13:36 +0500

    Project Initiaied

---
+++ MYSQL/Regular-Expression.php
@@ -0,0 +1,13 @@
+^           '^ra'               Beginning of string
+$           'an$'               End of string
+[...]       '[rms]'             Any charactor listed between the square brackets
+^[...]      '^[rms]'            Begins with Any character listed between the square brackets
+[a-z]       '[a-h]e'            Match with in the range
+p1|p2|p3    'tom|dick|harry'    matches any of the patterns p1, p2, or p3
+
+
+select column1, column2, column3,.....;
+
+FROM table_name
+
+WHERE column_name REGEXP pattern;
