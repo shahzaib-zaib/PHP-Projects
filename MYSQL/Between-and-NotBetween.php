3d3cb4783fd387c10229a8f724bbbb4e28f60432
Shahzaib <shahzaibux@gmail.com>
2020-06-21 02:29:12 +0500

    Project Initiaied

---
+++ MYSQL/Between-and-NotBetween.php
@@ -0,0 +1,25 @@
+select column1, column2, column3,.....;
+
+FROM table_name
+
+WHERE column_name BETWEEN value1 and value2;
+
+
+
+select column1, column2, column3,.....;
+
+FROM table_name
+
+WHERE column_name NOT BETWEEN value1 and value2;
+
+
+
+select * from table_name
+
+where age BETWEEN 18 and 20;
+
+
+
+select * from table_name
+
+where name BETWEEN "a" and "k";
