d6480bdb14b046b8ef590d787ddb710c5a0ec487
Shahzaib <shahzaibux@gmail.com>
2020-06-21 03:42:11 +0500

    Project Initiaied

---
+++ MYSQL/Count-Sum-Min-Max-Avg.php
@@ -0,0 +1,20 @@
+select COUNT(column_name)
+From table_name
+Where condition;
+
+select COUNT(DISTINCT city) as Total from table_name;
+
+
+select MIN(column_name)
+From table_name
+Where condition;
+
+
+select MAX(column_name)
+From table_name
+Where condition;
+
+
+select AVG(column_name)
+From table_name
+Where condition;
