571d06e0f9b058ea4bef0a03314954094d932b1a
Shahzaib <shahzaibux@gmail.com>
2020-06-21 03:35:36 +0500

    Project Initiaied

---
+++ MYSQL/Limit-and-Offset.php
@@ -0,0 +1,19 @@
+select column1, column2, column3,.....;
+
+FROM table_name
+
+WHERE condition
+
+LIMIT number;
+
+
+
+
+
+select column1, column2, column3,.....;
+
+FROM table_name
+
+WHERE condition
+
+LIMIT offset, number;
