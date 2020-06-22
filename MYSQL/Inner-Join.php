172cc1d7290a5fe7dac511735646a100cedafb84
Shahzaib <shahzaibux@gmail.com>
2020-06-23 00:55:20 +0500

    Project Initiaied

---
+++ MYSQL/Inner-Join.php
@@ -0,0 +1,14 @@
+SELECT columns
+
+FROM table1
+
+INNER JOIN table2
+
+ON table1.cloumn_name = table2.column_name
+            foreign key         Primary key
+
+
+SELECT * FROM personal p INNER JOIN city c ON p.city = c.cid;
+
+
+SELECT p.id,p.name,p.percentage,p.age,p.gender,c.cityname FROM personal p INNSER JOIN city c ON p.city = c.cid;
