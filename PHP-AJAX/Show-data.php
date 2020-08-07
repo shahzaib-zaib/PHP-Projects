0ef80025395af735416c5a0db3342c855f769f68
Shahzaib <shahzaibux@gmail.com>
2020-08-08 04:59:16 +0500

    Updated Project

---
+++ PHP-AJAX/Show-data.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>ADMIN Panel</title>
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="../css/bootstrap.min.css" />
+        <!-- Font Awesome Icon -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <!-- Custom stlylesheet -->
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    <body>
+        <!-- HEADER -->
+        <div id="header-admin">
+            <!-- container -->
+            <div class="container">
+                <!-- row -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>PHP with AJAX</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /HEADER -->
+        <!-- Menu Bar -->
+        <div id="admin-menubar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                       <ul class="admin-menu">
+                            <li>
+                                <a href="post.php">Post</a>
+                            </li>
+                            <li>
+                                <a href="category.php">Category</a>
+                            </li>
+                            <li>
+                                <a href="users.php">Users</a>
+                            </li>
+                            <li>
+                                <a href="settings.php">Setting</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /Menu Bar -->
+
+</body>
+</html>
