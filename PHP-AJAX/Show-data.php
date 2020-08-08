26f506bac7c35d506abe6234ce5ce394a6c132d4
Shahzaib <shahzaibux@gmail.com>
2020-08-08 05:22:25 +0500

    Updated Project

--- PHP-AJAX/Show-data.php
+++ PHP-AJAX/Show-data.php
@@ -3,90 +3,88 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>ADMIN Panel</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Custom stlylesheet -->
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <title>ADMIN Panel</title>
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="css/bootstrap.min.css" />
-        <!-- Font Awesome Icon -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <!-- Custom stlylesheet -->
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
-        <!-- HEADER -->
-        <div id="header-admin">
-            <!-- container -->
-            <div class="container">
-                <!-- row -->
-                <div class="row">
-                    <div class="col-md-12">
-                        <h1>PHP with AJAX</h1>
-                    </div>
+    <!-- HEADER -->
+    <div id="header-admin">
+        <!-- container -->
+        <div class="container">
+            <!-- row -->
+            <div class="row">
+                <div class="text-center">
+                    <h1>PHP with AJAX</h1>
                 </div>
             </div>
         </div>
-        <!-- /HEADER -->
-        <!-- Menu Bar -->
-        <div id="admin-menubar">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                       <ul class="admin-menu">
-                            <li>
-                                <a href="post.php">Post</a>
-                            </li>
-                            <li>
-                                <a href="category.php">Category</a>
-                            </li>
-                            <li>
-                                <a href="users.php">Users</a>
-                            </li>
-                            <li>
-                                <a href="settings.php">Setting</a>
-                            </li>
-                        </ul>
-                    </div>
+    </div>
+    <!-- /HEADER -->
+    <!-- Menu Bar -->
+    <div id="admin-menubar">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="admin-menu">
+                        <li>
+                            <a href="post.php">Post</a>
+                        </li>
+                        <li>
+                            <a href="category.php">Category</a>
+                        </li>
+                        <li>
+                            <a href="users.php">Users</a>
+                        </li>
+                        <li>
+                            <a href="settings.php">Setting</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
-        <!-- /Menu Bar -->
-  <div id="admin-content">
-      <div class="container">
-          <div class="row">
-              <div class="col-md-10">
-                  <h1 class="admin-heading">All Posts</h1>
-              </div>
-              <div class="col-md-2">
-                  <a class="add-new" href="add-post.php">add post</a>
-              </div>
-              <div class="col-md-12">
-                  <table class="content-table">
-                      <thead>
-                          <th>S.No.</th>
-                          <th>Title</th>
-                          <th>Category</th>
-                          <th>Date</th>
-                          <th>Author</th>
-                          <th>Edit</th>
-                          <th>Delete</th>
-                      </thead>
-                      <tbody>
-                      </tbody>
-                  </table>
-              </div>
-          </div>
-      </div>
-  </div>
+    </div>
+    <!-- /Menu Bar -->
+    <div id="admin-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10">
+                    <h1 class="admin-heading">All Posts</h1>
+                </div>
+                <div class="col-md-2">
+                    <a class="add-new" href="add-post.php">add post</a>
+                </div>
+                <div class="col-md-12">
+                    <table class="content-table">
+                        <thead>
+                            <th>ID</th>
+                            <th>Name</th>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer -->
+    <div id ="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <span>© Copyright 2020 News | Powered by Shahzaib</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /Footer -->
 
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js"></script>
 </body>
 </html>
